b6eef9dfb19e4becfe3825b70b6d021ec41b418c
Gagandeep Singh <68263387+Gagandeep627@users.noreply.github.com>
2022-01-22 17:45:08 +0530

    updted3//
    updted3//

--- app2.js
+++ app2.js
@@ -8,9 +8,13 @@
 
 // 3)style the chat app more awesome
 
-
-
 let text = ["your name :", "rohan", "your age: ", "25"];
+
+// loader
+
+const server = document.querySelector(".server");
+
+const load = document.querySelector(".loader-main");
 
 // let ids = ["111", "222", "333"];
 
@@ -19,6 +23,8 @@ let count = 0;
 const content = document.querySelector(".content-a");
 
 function additem() {
+  // clearintern();
+
   if (count < text.length) {
     const div = document.createElement("div");
 
@@ -33,14 +39,23 @@ function additem() {
       div.classList.add("getaway");
     }
 
-    content.appendChild(div);
+    remove_loader();
 
-    // console.log(div.textContent);
+    content.appendChild(div);
+    const mytimeout = setTimeout(loader(), 1000);
+
+    // adding load interval after each message-->
 
     ++count;
   } else {
     console.error("error obtained !");
     clearInterval(myTimer);
+    loader();
+    // clearintern();
+    // add server lefted the origin chat-->
+    close_server();
+    
+
   }
 }
 
@@ -55,3 +70,30 @@ function random() {
   return randin;
 }
 // random();
+
+// after message show loader-->
+// before message hide loader-->
+
+function loader() {
+  load.classList.toggle("hide");
+}
+
+function remove_loader() {
+  if (load.classList.contains("hide")) {
+    load.classList.add("hide");
+  }
+}
+
+function clearintern() {
+  clearTimeout(mytimeout);
+}
+
+
+// showing server left when all items are added means when console catches the error-->
+
+function close_server() {
+
+  server.removeAttribute("id");
+
+  // server.id.remove("hide");
+}
